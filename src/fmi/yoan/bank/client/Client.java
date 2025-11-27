e7325e672ae3daa92d6273dc5cb9da165c0b9d1e
Yoan Ivanov <yoan_ivanov1@yahoo.com>
2025-11-27 14:07:24 +0200

    Client and Server implementation

--- src/fmi/yoan/bank/client/Client.java
+++ src/fmi/yoan/bank/client/Client.java
@@ -1,4 +1,52 @@
 package fmi.yoan.bank.client;
 
+import fmi.yoan.bank.server.Bank;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
 public class Client {
+    private static final String SERVER_IP = "127.0.0.1";
+    private static final int SERVER_PORT = 6262;
+
+    public static  void main(String[] args) {
+        System.out.println("[Client] Connected to Server...");
+
+        try(Socket socket = new Socket(SERVER_IP, SERVER_PORT)) {
+            System.out.println("[Client] Connected to Server " + SERVER_IP + ":" + SERVER_PORT);
+
+            PrintWriter serverOutput = new PrintWriter(socket.getOutputStream(), true);
+            BufferedReader serverInput = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            Scanner consoleInput = new Scanner(System.in);
+
+            Thread listnerThread = new Thread(() -> {
+                try {
+                    String serverResponse;
+                    while ((serverResponse = serverInput.readLine()) != null) {
+                        System.out.println(serverResponse);
+                    }
+                } catch (IOException e) {
+                    System.err.println("[Client] Error: " + e.getMessage());
+                }
+            });
+            listnerThread.start();
+
+            while(true) {
+                String cmd = consoleInput.nextLine();
+                serverOutput.println(cmd);
+
+                if("EXIT".equalsIgnoreCase(cmd)) {
+                    System.out.println("[Client] Exiting...");
+                    break;
+                }
+            }
+
+        } catch (IOException e) {
+            System.err.println("[Error] Could not connect to the server." + e.getMessage());
+        }
+    }
 }
--- src/fmi/yoan/bank/server/Server.java
+++ src/fmi/yoan/bank/server/Server.java
@@ -1,4 +1,55 @@
 package fmi.yoan.bank.server;
 
+import java.net.*;
+import java.io.*;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
 public class Server {
+    private static final int PORT = 6262;
+
+   public static void main(String[] args) {
+       Bank bank = new Bank("YoanBank", "YB", 10000000.00);
+
+       System.out.println("[System] Bank created successfully!");
+       System.out.println("Bank reserves: " + bank.getBankReserves());
+
+        startInterestTimer(bank);
+
+        ExecutorService clientThreadPool = Executors.newCachedThreadPool();
+
+        try(ServerSocket serverSocket = new ServerSocket((PORT))) {
+            System.out.println("[System] Server listening on port " + PORT + "...");
+
+            while (true) {
+                Socket clientSocket = serverSocket.accept();
+                System.out.println("[System] Accepted connection from " + clientSocket.getInetAddress());
+
+                ClientHandler clientHandler = new ClientHandler(clientSocket, bank);
+
+                clientThreadPool.execute(clientHandler);
+            }
+        } catch (IOException e) {
+            System.err.println("[Error] " +  e.getMessage());
+            e.printStackTrace();
+        }
+   }
+
+   private static void startInterestTimer(Bank bank) {
+        ScheduledExecutorService scheduler = Executors.newSingleThreadScheduledExecutor();
+
+        Runnable interestTask = () -> {
+            try {
+                bank.runDailyPenaltyCheck();
+            } catch (Exception e) {
+                System.err.println("[Error] " +  e.getMessage());
+            }
+        };
+
+        scheduler.scheduleAtFixedRate(interestTask,10, 60, TimeUnit.SECONDS);
+
+        System.out.println("[System] Penalty check started! (1 min = 1 day");
+   }
 }
